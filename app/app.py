ecdc08ea4e0f4131ef0d2f0c5ce014cf193fbae3
tiagojleandro <leandrotiago223@gmail.com>
2021-07-04 20:53:04 -0300

    criando um contexto para o shell e executando o create_app

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from os import environ
+from . import create_app, db
+from .models import User
+
+
+config_name = environ.get("APP_CONFIG", "default")
+app = create_app(config_name)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User)
