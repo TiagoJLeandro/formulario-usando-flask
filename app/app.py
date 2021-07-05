98b01b9b4811c94330f355d64d6cc96ab3f63cd8
tiagojleandro <leandrotiago223@gmail.com>
2021-07-04 22:49:06 -0300

    adicionando address na funcao make_shell_context

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from os import environ
 from . import create_app, db
-from .models import User
+from .models import User, Address
 
 
 config_name = environ.get("APP_CONFIG", "default")
@@ -8,4 +8,4 @@ app = create_app(config_name)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User)
+    return dict(db=db, user=User, address=Address)
