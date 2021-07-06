0d9359b0019b8768d7f1e79e3502560f4fb2029e
TiagoJLeandro <leandrotiago223@gmail.com>
2021-07-06 09:06:07 -0300

    adicionando o formulario de login

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,22 +3,32 @@ from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, EqualTo, Email, Length
 
 
-class RegisterForm(FlaskForm):
-    name = StringField(validators=[DataRequired(), Length(min=4, max=128)])
-    email = StringField(validators=[DataRequired(), Email(max=128)])
+class LoginForm(FlaskForm):
+    email = StringField(validators=[DataRequired(), Email(), Length])
     password = PasswordField(
         validators=[
             DataRequired(),
+            Length(5, 20)
+        ]
+    )
+
+
+class RegisterForm(FlaskForm):
+    name = StringField(validators=[DataRequired(), Length(4, 128)])
+    email = StringField(validators=[DataRequired(), Email(), Length(max=128)])
+    password = PasswordField(
+        validators=[
+            DataRequired(),
+            Length(5, 20),
             EqualTo(
                 fieldname="password2",
                 message="As senhas precisam ser identicas."
-            ),
-            Length(min=5, max=20)
+            )
         ]
     )
     password2 = PasswordField(validators=[DataRequired()])
-    cep = StringField(validators=[DataRequired(), Length(min=9, max=9)])
+    cep = StringField(validators=[DataRequired(), Length(9, 9)])
     rua = StringField(validators=[DataRequired(), Length(max=128)])
     bairro = StringField(validators=[DataRequired(), Length(max=128)])
     cidade = StringField(validators=[DataRequired(), Length(max=128)])
-    uf = StringField(validators=[DataRequired(), Length(min=2, max=2)])
+    uf = StringField(validators=[DataRequired(), Length(2)])
