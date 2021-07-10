94ebd5c9b977861994759e98feb0bd092cf688f0
tiagojleandro <leandrotiago223@gmail.com>
2021-07-10 00:50:43 -0300

    adicionando o flash para a exibiçao de msg

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,14 +1,23 @@
 from app.auth import auth
-from flask import render_template, redirect, url_for
-from flask_login import login_required, logout_user
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_required, logout_user, login_user
 from .forms import LoginForm
+from ..models import User
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('main.index')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get("next")
+            if next is None or not next.startswith("/"):
+                next = url_for('main.index')
+            return redirect(next)
+
+        flash('Email ou Senha inválida.')
 
     return render_template("auth/login.html", form=form)
 
