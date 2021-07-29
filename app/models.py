6c230f55d20d49f4751ca36975d8ce7754dba148
tiagojleandro <leandrotiago223@gmail.com>
2021-07-29 06:38:38 -0300

    adicionando uma funcao para gerar e outra para confirmar um token de autenticacao

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
+from flask.globals import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 class User(UserMixin, db.Model):
@@ -24,6 +26,24 @@ class User(UserMixin, db.Model):
     
     def verify_password(self, password) -> bool:
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        
+        if data.get("confirm") != self.id:
+            return False
+        
+        self.enable = True
+        db.session.add(self)
+        return True
     
     def __repr__(self):
         return f"User(Id: {self.id}, Name: {self.name}, " \
